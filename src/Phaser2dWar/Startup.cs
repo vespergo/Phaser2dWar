82307df9826ef756578904d70005eb03b1892973
Josh O'Bryan <vespergo@gmail.com>
2016-06-04 17:35:33 -0700

    baseline

--- src/Phaser2dWar/Startup.cs
+++ src/Phaser2dWar/Startup.cs
@@ -15,14 +15,19 @@ namespace Phaser2dWar
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
---
+++ src/Phaser2dWar/wwwroot/scripts/main.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="phaser.js" />
+var game = new Phaser.Game(1000, 700, Phaser.CANVAS, 'main', { preload: preload, create: create });
+var inTxt;
+
+function preload() {
+    game.load.image('background', 'images/background.jpg');
+    game.load.spritesheet('planets', 'images/planets.png', 275, 275);
+    
+}
+
+
+function create() {
+    game.add.sprite(0, 0, 'background');
+    var homePlanet = game.add.sprite(game.world.centerX, game.height - 100, 'planets', 7);
+    homePlanet.anchor.set(0.5);
+    homePlanet.scale.set(0.3);
+
+    inTxt = game.add.text(game.world.centerX, 10, "Place your starting world.", { fill: 'white' });    
+    inTxt.anchor.set(0.5, 0);
+}
+
